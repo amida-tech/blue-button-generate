018f63de6398cfa47c628e35bcc6c3c4b7fb855a
au2 <afsin@amida-tech.com>
2014-10-10 14:05:46 -0400

    payers section template first version completed

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ var gen = function (data, CCD, xmlDoc, section_name) {
         } else if (section_name === "medications") {
             js2xml.fillUsingTemplate(xmlDoc, data, sectionLevel.medicationsSectionEntriesRequired);
         } else if (section_name === "payers") {
-            return require('./lib/payers.js')(data, codeSystems, CCD, xmlDoc);
+            js2xml.fillUsingTemplate(xmlDoc, data, sectionLevel.payersSectionEntriesRequired);
         } else if (section_name === "plan_of_care") {
             js2xml.fillUsingTemplate(xmlDoc, data, sectionLevel.planOfCareSectionEntriesRequired);
         } else if (section_name === "problems") {
@@ -162,7 +162,7 @@ var genWholeCCDA = function (data) {
         var sb = xmlDoc.node('component').node('structuredBody');
         // loop over all the sections and generate each one, adding them iteratively to each other
         for (var i = 2; i < Object.keys(sectionNames).length; i++) {
-            if ((sectionNames[i] === 'plan_of_care') || (sectionNames[i] === 'procedures') || (sectionNames[i] === 'encounters') || (sectionNames[i] === 'allergies') || (sectionNames[i] === 'medications') || (sectionNames[i] === 'immunizations') || (sectionNames[i] === 'problems')) {
+            if ((sectionNames[i] === 'payers') || (sectionNames[i] === 'plan_of_care') || (sectionNames[i] === 'procedures') || (sectionNames[i] === 'encounters') || (sectionNames[i] === 'allergies') || (sectionNames[i] === 'medications') || (sectionNames[i] === 'immunizations') || (sectionNames[i] === 'problems')) {
                 gen(data, true, sb, sectionNames[i]);
             } else {
                 gen(data[sectionNames[i]], true, sb, sectionNames[i]);
--- test/xmlmods/ccd1Generator.js
+++ test/xmlmods/ccd1Generator.js
@@ -54,4 +54,12 @@ module.exports = [{
         xpath: "h:title",
         comment: "titles may differ"
     }]
+}, {
+    xpath: "2.16.840.1.113883.10.20.22.2.18",
+    description: "Payers Section (entries required)",
+    type: "rootTemplate",
+    childxpaths: [{
+        xpath: "h:title",
+        comment: "titles may differ"
+    }]
 }];
