2da8d32542767d3775f5500c2e73760be6336489
au2 <afsin@amida-tech.com>
2014-10-10 06:58:51 -0400

    plan of care template

--- test/xmlmods/ccd1Generator.js
+++ test/xmlmods/ccd1Generator.js
@@ -46,4 +46,12 @@ module.exports = [{
         xpath: "h:title",
         comment: "titles may differ"
     }]
+}, {
+    xpath: "2.16.840.1.113883.10.20.22.2.10",
+    description: "Plan of Care Section (entries required)",
+    type: "rootTemplate",
+    childxpaths: [{
+        xpath: "h:title",
+        comment: "titles may differ"
+    }]
 }];
