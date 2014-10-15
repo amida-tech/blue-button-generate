df1bcf495f08862cf02968f1fb045664aac868d2
au2 <afsin@amida-tech.com>
2014-10-15 09:09:29 -0400

    infrastructure for text is put in place, code cleaning

--- index.js
+++ index.js
@@ -6,11 +6,30 @@ standard XML/CCDA format) back to XML/CCDA format.
 var js2xml = require('./lib/js2xml');
 var documentLevel = require('./lib/documentLevel');
 
+var createContext = (function () {
+    var base = {
+        getReference: function (referenceKey) {
+            var index = this.references[referenceKey] || 0;
+            ++index;
+            this.references[referenceKey] = index;
+            return "#" + referenceKey + index;
+        }
+    };
+
+    return function () {
+        var result = Object.create(base);
+        result.references = {};
+        return result;
+    };
+})();
+
 var generate = function (input) {
     var data = input.data ? input.data : input;
     data.identifiers = input.meta && input.meta.identifiers;
 
-    return js2xml.create(documentLevel.ccd, data);
+    var context = createContext();
+
+    return js2xml.create(documentLevel.ccd, data, context);
 };
 
 module.exports = generate;
