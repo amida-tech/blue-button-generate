fa8b9f0c8a00948fa072b1cce7528bf8964145c7
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2014-11-17 09:35:19 -0500

    test reorganization/rename, cmd test also uses json

--- test/sample_runs/test-gen-parse-gen.js
+++ test/sample_runs/test-gen-parse-gen.js
@@ -173,4 +173,41 @@ describe('parse generate parse generate', function () {
 
         assert.deepEqual(result2.data, result.data);
     });
+
+    it('skewed sample data from app should still be same', function () {
+        //var data = fs.readFileSync("./sample.JSON").toString();
+
+        var data = fs.readFileSync(__dirname + "/../fixtures/json/sample.JSON");
+
+        //convert string into JSON 
+        var result = JSON.parse(data);
+
+        // write generated json
+        fs.writeFileSync(path.join(generatedDir, "CCD_1_generated_new.json"), JSON.stringify(result, null, 4));
+
+        // check validation
+        var val = bb.validator.validateDocumentModel(result);
+
+        // generate ccda
+
+        //console.log(result.demographics);
+        var xml = bbg.generateCCD(result);
+
+        // write ccda
+        fs.writeFileSync(path.join(generatedDir, "CCD_1_generated_new.xml"), xml);
+
+        // parse generated ccda
+        var result2 = bb.parseString(xml);
+        // write the parsed json from the generated ccda
+        fs.writeFileSync(path.join(generatedDir, "CCD_1_generated_2_new.json"), JSON.stringify(result2, null, 4));
+
+        // re-generate
+        var xml2 = bbg.generateCCD(result2);
+        fs.writeFileSync(path.join(generatedDir, "CCD_1_generated_2_new.xml"), xml2);
+
+        delete result.errors;
+        delete result2.errors;
+
+        //assert.deepEqual(result2, result);
+    });
 });
