13e084d0f8416867c38745ffd02b01c42eb58a7a
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-03-11 18:59:47 -0400

    unique-ids for entries

--- dist/blue-button-generate.js
+++ dist/blue-button-generate.js
@@ -251,7 +251,7 @@ var fillContent = function (node, input, context, template) {
 
 var updateUsingTemplate = function updateUsingTemplate(xmlDoc, input, context, template) {
     var condition = template.existsWhen;
-    if ((!condition) || condition(input)) {
+    if ((!condition) || condition(input, context)) {
         var name = template.key;
         var text = expandText(input, template);
         if (((text !== null) && (text !== undefined)) || template.content || template.attributes) {
@@ -460,6 +460,7 @@ var allergyProblemAct = exports.allergyProblemAct = {
     },
     content: [
         fieldLevel.templateId("2.16.840.1.113883.10.20.22.4.30"),
+        fieldLevel.uniqueId,
         fieldLevel.id,
         fieldLevel.templateCode("AllergyProblemAct"),
         fieldLevel.statusCodeActive, [fieldLevel.effectiveTime, required], {
@@ -2264,6 +2265,7 @@ exports.vitalSignsOrganizer = {
 "use strict";
 
 var bbm = require("blue-button-meta");
+var uuid = require('uuid');
 
 var condition = require("./condition");
 var leafLevel = require("./leafLevel");
@@ -2316,6 +2318,21 @@ var id = exports.id = {
     dataKey: 'identifiers',
     existsWhen: condition.keyExists('identifier'),
     required: true
+};
+
+exports.uniqueId = {
+    key: "id",
+    attributes: {
+        root: function (input, context) {
+            return context.rootId;
+        },
+        extension: function () {
+            return uuid.v4();
+        }
+    },
+    existsWhen: function (input, context) {
+        return context.rootId;
+    }
 };
 
 exports.statusCodeCompleted = {
@@ -2502,7 +2519,7 @@ exports.performer = {
     dataKey: "performer"
 };
 
-},{"./condition":2,"./contentModifier":3,"./leafLevel":21,"./translate":23,"blue-button-meta":24}],20:[function(require,module,exports){
+},{"./condition":2,"./contentModifier":3,"./leafLevel":21,"./translate":23,"blue-button-meta":24,"uuid":43}],20:[function(require,module,exports){
 "use strict";
 
 var fieldLevel = require('./fieldLevel');
@@ -17739,7 +17756,227 @@ exports.or = function (fns) {
 }).call(this);
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],"blue-button-generate":[function(require,module,exports){
+},{}],42:[function(require,module,exports){
+(function (global){
+
+var rng;
+
+if (global.crypto && crypto.getRandomValues) {
+  // WHATWG crypto-based RNG - http://wiki.whatwg.org/wiki/Crypto
+  // Moderately fast, high quality
+  var _rnds8 = new Uint8Array(16);
+  rng = function whatwgRNG() {
+    crypto.getRandomValues(_rnds8);
+    return _rnds8;
+  };
+}
+
+if (!rng) {
+  // Math.random()-based (RNG)
+  //
+  // If all else fails, use Math.random().  It's fast, but is of unspecified
+  // quality.
+  var  _rnds = new Array(16);
+  rng = function() {
+    for (var i = 0, r; i < 16; i++) {
+      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
+      _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
+    }
+
+    return _rnds;
+  };
+}
+
+module.exports = rng;
+
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],43:[function(require,module,exports){
+//     uuid.js
+//
+//     Copyright (c) 2010-2012 Robert Kieffer
+//     MIT License - http://opensource.org/licenses/mit-license.php
+
+// Unique ID creation requires a high quality random # generator.  We feature
+// detect to determine the best RNG source, normalizing to a function that
+// returns 128-bits of randomness, since that's what's usually required
+var _rng = require('./rng');
+
+// Maps for number <-> hex string conversion
+var _byteToHex = [];
+var _hexToByte = {};
+for (var i = 0; i < 256; i++) {
+  _byteToHex[i] = (i + 0x100).toString(16).substr(1);
+  _hexToByte[_byteToHex[i]] = i;
+}
+
+// **`parse()` - Parse a UUID into it's component bytes**
+function parse(s, buf, offset) {
+  var i = (buf && offset) || 0, ii = 0;
+
+  buf = buf || [];
+  s.toLowerCase().replace(/[0-9a-f]{2}/g, function(oct) {
+    if (ii < 16) { // Don't overflow!
+      buf[i + ii++] = _hexToByte[oct];
+    }
+  });
+
+  // Zero out remaining bytes if string was short
+  while (ii < 16) {
+    buf[i + ii++] = 0;
+  }
+
+  return buf;
+}
+
+// **`unparse()` - Convert UUID byte array (ala parse()) into a string**
+function unparse(buf, offset) {
+  var i = offset || 0, bth = _byteToHex;
+  return  bth[buf[i++]] + bth[buf[i++]] +
+          bth[buf[i++]] + bth[buf[i++]] + '-' +
+          bth[buf[i++]] + bth[buf[i++]] + '-' +
+          bth[buf[i++]] + bth[buf[i++]] + '-' +
+          bth[buf[i++]] + bth[buf[i++]] + '-' +
+          bth[buf[i++]] + bth[buf[i++]] +
+          bth[buf[i++]] + bth[buf[i++]] +
+          bth[buf[i++]] + bth[buf[i++]];
+}
+
+// **`v1()` - Generate time-based UUID**
+//
+// Inspired by https://github.com/LiosK/UUID.js
+// and http://docs.python.org/library/uuid.html
+
+// random #'s we need to init node and clockseq
+var _seedBytes = _rng();
+
+// Per 4.5, create and 48-bit node id, (47 random bits + multicast bit = 1)
+var _nodeId = [
+  _seedBytes[0] | 0x01,
+  _seedBytes[1], _seedBytes[2], _seedBytes[3], _seedBytes[4], _seedBytes[5]
+];
+
+// Per 4.2.2, randomize (14 bit) clockseq
+var _clockseq = (_seedBytes[6] << 8 | _seedBytes[7]) & 0x3fff;
+
+// Previous uuid creation time
+var _lastMSecs = 0, _lastNSecs = 0;
+
+// See https://github.com/broofa/node-uuid for API details
+function v1(options, buf, offset) {
+  var i = buf && offset || 0;
+  var b = buf || [];
+
+  options = options || {};
+
+  var clockseq = options.clockseq !== undefined ? options.clockseq : _clockseq;
+
+  // UUID timestamps are 100 nano-second units since the Gregorian epoch,
+  // (1582-10-15 00:00).  JSNumbers aren't precise enough for this, so
+  // time is handled internally as 'msecs' (integer milliseconds) and 'nsecs'
+  // (100-nanoseconds offset from msecs) since unix epoch, 1970-01-01 00:00.
+  var msecs = options.msecs !== undefined ? options.msecs : new Date().getTime();
+
+  // Per 4.2.1.2, use count of uuid's generated during the current clock
+  // cycle to simulate higher resolution clock
+  var nsecs = options.nsecs !== undefined ? options.nsecs : _lastNSecs + 1;
+
+  // Time since last uuid creation (in msecs)
+  var dt = (msecs - _lastMSecs) + (nsecs - _lastNSecs)/10000;
+
+  // Per 4.2.1.2, Bump clockseq on clock regression
+  if (dt < 0 && options.clockseq === undefined) {
+    clockseq = clockseq + 1 & 0x3fff;
+  }
+
+  // Reset nsecs if clock regresses (new clockseq) or we've moved onto a new
+  // time interval
+  if ((dt < 0 || msecs > _lastMSecs) && options.nsecs === undefined) {
+    nsecs = 0;
+  }
+
+  // Per 4.2.1.2 Throw error if too many uuids are requested
+  if (nsecs >= 10000) {
+    throw new Error('uuid.v1(): Can\'t create more than 10M uuids/sec');
+  }
+
+  _lastMSecs = msecs;
+  _lastNSecs = nsecs;
+  _clockseq = clockseq;
+
+  // Per 4.1.4 - Convert from unix epoch to Gregorian epoch
+  msecs += 12219292800000;
+
+  // `time_low`
+  var tl = ((msecs & 0xfffffff) * 10000 + nsecs) % 0x100000000;
+  b[i++] = tl >>> 24 & 0xff;
+  b[i++] = tl >>> 16 & 0xff;
+  b[i++] = tl >>> 8 & 0xff;
+  b[i++] = tl & 0xff;
+
+  // `time_mid`
+  var tmh = (msecs / 0x100000000 * 10000) & 0xfffffff;
+  b[i++] = tmh >>> 8 & 0xff;
+  b[i++] = tmh & 0xff;
+
+  // `time_high_and_version`
+  b[i++] = tmh >>> 24 & 0xf | 0x10; // include version
+  b[i++] = tmh >>> 16 & 0xff;
+
+  // `clock_seq_hi_and_reserved` (Per 4.2.2 - include variant)
+  b[i++] = clockseq >>> 8 | 0x80;
+
+  // `clock_seq_low`
+  b[i++] = clockseq & 0xff;
+
+  // `node`
+  var node = options.node || _nodeId;
+  for (var n = 0; n < 6; n++) {
+    b[i + n] = node[n];
+  }
+
+  return buf ? buf : unparse(b);
+}
+
+// **`v4()` - Generate random UUID**
+
+// See https://github.com/broofa/node-uuid for API details
+function v4(options, buf, offset) {
+  // Deprecated - 'format' argument, as supported in v1.2
+  var i = buf && offset || 0;
+
+  if (typeof(options) == 'string') {
+    buf = options == 'binary' ? new Array(16) : null;
+    options = null;
+  }
+  options = options || {};
+
+  var rnds = options.random || (options.rng || _rng)();
+
+  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+  rnds[6] = (rnds[6] & 0x0f) | 0x40;
+  rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+  // Copy bytes to buffer, if provided
+  if (buf) {
+    for (var ii = 0; ii < 16; ii++) {
+      buf[i + ii] = rnds[ii];
+    }
+  }
+
+  return buf || unparse(rnds);
+}
+
+// Export public API
+var uuid = v4;
+uuid.v1 = v1;
+uuid.v4 = v4;
+uuid.parse = parse;
+uuid.unparse = unparse;
+
+module.exports = uuid;
+
+},{"./rng":42}],"blue-button-generate":[function(require,module,exports){
 "use strict";
 
 /*
@@ -17747,8 +17984,12 @@ This script converts CCDA data in JSON format (originally generated from a Conti
 standard XML/CCDA format) back to XML/CCDA format.
 */
 
+var bbu = require("blue-button-util");
+
 var engine = require('./lib/engine');
 var documentLevel = require('./lib/documentLevel');
+
+var bbuo = bbu.object;
 
 var createContext = (function () {
     var base = {
@@ -17764,22 +18005,30 @@ var createContext = (function () {
         }
     };
 
-    return function () {
+    return function (options) {
         var result = Object.create(base);
         result.references = {};
+        if (options.meta && options.addUniqueIds) {
+            result.rootId = bbuo.deepValue(options.meta, 'identifiers.0.identifier');
+        } else {
+            result.rootId = null;
+        }
+
         return result;
     };
 })();
 
-var generate = exports.generate = function (template, input) {
-    var context = createContext();
+var generate = exports.generate = function (template, input, options) {
+    var context = createContext(options);
     return engine.create(documentLevel.ccd, input, context);
 };
 
-exports.generateCCD = function (input) {
+exports.generateCCD = function (input, options) {
+    options = options || {};
     var data = input.data ? input.data : input;
     data.identifiers = input.meta && input.meta.identifiers;
-    return generate(documentLevel.ccd, data);
+    options.meta = input.meta;
+    return generate(documentLevel.ccd, data, options);
 };
 
-},{"./lib/documentLevel":4,"./lib/engine":5}]},{},["blue-button-generate"]);
+},{"./lib/documentLevel":4,"./lib/engine":5,"blue-button-util":34}]},{},["blue-button-generate"]);
